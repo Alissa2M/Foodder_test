f5dc1813a3bc99ceb52924ea22dde1d126a66661
Alissa2M <alissa2021k@gmail.com>
2023-04-02 01:03:33 +0900

    pwa修正 #33

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,15 +21,3 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
-
-window.addEventListener('load', () => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').
-        then(() => {
-          console.log('ServiceWorker registered')
-        }).
-        catch((error) => {
-          console.warn('ServiceWorker error', error)
-        })
-    }
-  });
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
@@ -15,6 +16,50 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'inline',
+            includeAssets: [
+              'offline.html',
+              'favicon.ico',
+              'robots.txt',
+            ],
+            manifest: {
+              theme_color: '#FFF3E0',
+              background_color: '#FFF3E0',
+              display: 'standalone',
+              scope: '/',
+              start_url: '/',
+              short_name: 'Foodder',
+              name: 'Foodder',
+              icons: [
+                {
+                    src: 'img/Foodder_icon_48.png',
+                    sizes: '48x48',
+                    type: 'image/png',
+                },
+                {
+                    src: 'img/Foodder_icon_96.png',
+                    sizes: '96x96',
+                    type: 'image/png',
+                },  
+                {
+                  src: 'img/icon-192x192.png',
+                  sizes: '192x192',
+                  type: 'image/png',
+                },
+                {
+                  src: 'img/Foodder_icon_512.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+                },
+              ],
+            },
+            workbox: {
+                cleanupOutdatedCaches: false,
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
+            }
         }),
     ],
 });
